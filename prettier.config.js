287a63026dcfc224ebe40d79343b3a5ae70933c7
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2020-12-26 06:01:18 -0300

    chore: add prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  singleQuote: true,
-  trailingComma: 'all',
   arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  quoteProps: 'as-needed',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false,
 };
