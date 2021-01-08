d99c8e6d73d3c6300c4260bccf8b908f3dc02f3f
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2021-01-07 22:58:12 -0300

    feat(lesson): usando controllers

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,23 +1,10 @@
 import { Router } from 'express'
-import AuthenticateUserService from 'modules/users/services/AuthenticateUserService'
-import UsersRepository from '../../typeorm/repositories/UsersRepository'
+
+import SessionsController from '../controllers/SessionsController'
 
 const sessionsRouter = Router()
+const sessionsController = new SessionsController()
 
-sessionsRouter.post('/', async (request, response) => {
-  const { email, password } = request.body
-
-  const usersRepository = new UsersRepository()
-  const authenticateUser = new AuthenticateUserService(usersRepository)
-
-  const { user, token } = await authenticateUser.execute({
-    email,
-    password,
-  })
-
-  const { password: omittedPassword, ...responseUser } = user
-
-  return response.json({ user: responseUser, token })
-})
+sessionsRouter.post('/', sessionsController.create)
 
 export default sessionsRouter
