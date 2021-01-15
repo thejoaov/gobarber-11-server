b41e3f57b3064ec29b15b88be7ba62fed2d9ab53
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2021-01-15 00:09:33 -0300

    feat(lesson): emails em desenvolvimento

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -18,7 +18,7 @@ const { PORT } = process.env
 
 app.use(cors())
 app.use(express.json())
-app.use('/files', express.static(uploadConfig.directory))
+app.use('/files', express.static(uploadConfig.uploadsFolder))
 app.use(routes)
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
