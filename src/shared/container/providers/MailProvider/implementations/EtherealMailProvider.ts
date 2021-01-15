3a34ddf7b325fefb495f5c1b4a999368a8cc9480
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2021-01-15 08:34:42 -0300

    feat(lesson): template de emails

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,12 +1,18 @@
-import AppError from '@shared/errors/AppError'
+import { injectable, inject } from 'tsyringe'
 import nodemailer, { Transporter } from 'nodemailer'
 
+import ISendMailDTO from '../dtos/ISendMailDTO'
 import IMailProvider from '../models/IMailProvider'
+import IMailTemplateProvider from '../../MailTemplateProvider/models/IMailTemplateProvider'
 
+@injectable()
 export default class EtherealMailProvider implements IMailProvider {
   private client: Transporter
 
-  constructor() {
+  constructor(
+    @inject('MailTemplateProvider')
+    private mailTemplateProvider: IMailTemplateProvider,
+  ) {
     nodemailer.createTestAccount().then(account => {
       const transporter = nodemailer.createTransport({
         host: account.smtp.host,
@@ -22,19 +28,26 @@ export default class EtherealMailProvider implements IMailProvider {
     })
   }
 
-  public async sendMail(to: string, body: string): Promise<void> {
-    try {
-      const message = await this.client.sendMail({
-        from: 'Equipe Gobarber <equipe@gobarber.com>',
-        to,
-        subject: 'Recuperação de senha',
-        text: body,
-      })
+  public async sendMail({
+    from,
+    to,
+    subject,
+    templateData,
+  }: ISendMailDTO): Promise<void> {
+    const message = await this.client.sendMail({
+      from: {
+        name: from?.name || 'Equipe GoBarber',
+        address: from?.email || 'equipe@gobarber.com.br',
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await this.mailTemplateProvider.parse(templateData),
+    })
 
-      console.log('Message sent: %s', message.messageId)
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message))
-    } catch (error) {
-      throw new AppError(`Failed to send email: ${error}`)
-    }
+    console.log('Message sent: %s', message.messageId)
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message))
   }
 }
