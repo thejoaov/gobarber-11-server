3d5627aacb633ccf76ab15b0be00c2250a4e69ad
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2021-01-22 02:23:27 -0300

    feat(lesson): organizando o container

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,35 +1,3 @@
-import { container } from 'tsyringe'
-
-import mailConfig from '@config/mail'
-
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider'
-import IStorageProvider from './StorageProvider/models/IStorageProvider'
-
-import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider'
-import SESMailProvider from './MailProvider/implementations/SESMailProvider'
-import IMailProvider from './MailProvider/models/IMailProvider'
-
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider'
-import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider'
-import MailhogMailProvider from './MailProvider/implementations/MailhogMailProvider'
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-)
-
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarsMailTemplateProvider,
-)
-
-const mailProviders = {
-  ethereal: container.resolve(EtherealMailProvider),
-  mailhog: container.resolve(MailhogMailProvider),
-  ses: container.resolve(SESMailProvider),
-}
-
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  mailProviders[mailConfig.driver],
-)
+import './StorageProvider'
+import './MailTemplateProvider'
+import './MailProvider'
