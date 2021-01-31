b76cc68aa370dafd113e640d41309cc34493efa1
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2021-01-31 09:51:35 -0300

    fix: pm2 instances to spawn in production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,11 @@
+require('dotenv/config')
+
 module.exports = {
   apps: [
     {
       script: 'dist/shared/infra/http/server.js',
       exec_mode: 'cluster',
-      instances: 'max',
+      instances: process.env.INSTANCES || 'max',
       env: {
         NODE_ENV: 'production',
       },
