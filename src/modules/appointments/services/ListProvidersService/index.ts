1b2d6e05a80095ed153a566aaf76747baf5e0c1a
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2021-02-09 13:27:46 -0300

    Update index.ts

--- src/modules/appointments/services/ListProvidersService/index.ts
+++ src/modules/appointments/services/ListProvidersService/index.ts
@@ -22,9 +22,8 @@ class ListProvidersService {
     /* let users = await this.cacheProvider.recover<User[]>(
       `providers-list:${user_id}`,
     ) */
+    let users = []
     
-    let users = [];
-
     if (!users) {
       users = await this.usersRepository.findAllProviders({
         except_user_id: user_id,
