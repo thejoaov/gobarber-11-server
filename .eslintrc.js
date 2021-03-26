cbd60071ce0ab3cfac6583974a1917441daab4f4
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2021-03-26 00:10:40 -0300

    chore: eslint plugin prettier update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
   extends: [
     'airbnb-base',
     'plugin:@typescript-eslint/recommended',
-    // 'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   globals: {
