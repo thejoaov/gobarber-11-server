d6a1b145ce26b63441dc6e38a7d2d75c45c1fb37
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2021-03-26 00:08:02 -0300

    chore: testing husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   extends: [
     'airbnb-base',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
+    // 'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   globals: {
--- src/modules/users/services/AuthenticateUserService/index.ts
+++ src/modules/users/services/AuthenticateUserService/index.ts
@@ -43,6 +43,7 @@ class AuthenticateUserService {
     if (!passwordMatched) {
       throw new AppError('Incorrect email/password combination', 401)
     }
+
     const { secret, expiresIn } = authConfig.jwt
 
     const token = sign({}, secret, {
