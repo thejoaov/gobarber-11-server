719a66fca46b6ff5ef6f1c2d35ae9ea0b6b94eee
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2021-03-28 09:39:49 -0300

    fix: test redis

--- src/config/cache.ts
+++ src/config/cache.ts
@@ -16,6 +16,7 @@ export default {
       host: process.env.REDIS_HOST,
       port: process.env.REDIS_PORT,
       password: process.env.REDIS_PASS || undefined,
+      username: process.env.REDIS_USER || undefined,
     },
   },
 } as ICacheConfig
