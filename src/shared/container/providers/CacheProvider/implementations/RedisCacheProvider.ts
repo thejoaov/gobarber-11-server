df45f81e101adb42c9e2f8e9ee9a5d688dba1482
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2021-03-28 09:06:52 -0300

    fix: fix redis connection

--- src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -12,7 +12,7 @@ export default class RedisCacheProvider implements ICacheProvider {
 
   constructor() {
     this.client =
-      process.env.NODE_ENV === 'production'
+      process.env.NODE_ENV === 'production' && !!process.env.REDIS_URL
         ? new Redis(process.env.REDIS_URL)
         : new Redis(cacheConfig.config.redis)
   }
