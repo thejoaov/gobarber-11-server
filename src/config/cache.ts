8cb9bf51697aa027b0921f486299919b1276eb3d
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2021-03-28 09:44:56 -0300

    fix: test redis

--- src/config/cache.ts
+++ src/config/cache.ts
@@ -17,6 +17,10 @@ export default {
       port: process.env.REDIS_PORT,
       password: process.env.REDIS_PASS || undefined,
       username: process.env.REDIS_USER || undefined,
+      reconnectOnError: (error): boolean => {
+        console.log('Always reconnect on error', error)
+        return true
+      },
     },
   },
 } as ICacheConfig
